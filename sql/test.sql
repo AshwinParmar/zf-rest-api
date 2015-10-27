105aab17d5ad11052ff707d1a4be145ec9041412
unknown <ashwin.parmar@DSK-021.gatewaygroup.com>
2015-10-27 13:10:00 +0530

    Added SQL Folder and Database dump of TEST Db.

---
+++ sql/test.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.6.17 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `test`;
+
+/*Table structure for table `foo` */
+
+DROP TABLE IF EXISTS `foo`;
+
+CREATE TABLE `foo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `value` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+/*Data for the table `foo` */
+
+insert  into `foo`(`id`,`name`,`value`) values (1,'first_name','Ashwin'),(2,'last_name','Parmar'),(3,'city','Jetpur'),(4,'country','India'),(5,'job','Software Engineer'),(6,'temp_path','/tmp'),(7,'A','B'),(8,'A1','B1');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
